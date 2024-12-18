88918c8689fdd01dc97026d6dbaf6736425d442b
brians031 <brian@agm-tec.com>
2024-12-18 12:35:53 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Action exécutée automatiquement au chargement de la page
+  window.location.href = "mailto:contact@agm-tec.com?subject=demande%20de%20contact&body=Bonjour,%0D%0A%0D%0AJe%20suis%20intéressé%20et%20je%20souhaiterai%20obtenir%20plus%20d’informations%20concernant%20votre%20matériel.%0D%0A%0D%0AMerci%20de%20me%20contacter%0D%0APar%20téléphone%20:%20%200561429063%0D%0APar%20mail%20:%20contact@agm-tec.com";
+});
