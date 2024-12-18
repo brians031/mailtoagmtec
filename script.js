4090a8c86895024a07cc98f2bcdafdaed70f89a6
brians031 <brian@agm-tec.com>
2024-12-18 13:39:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-window.location.href = "mailto:contact@agm-tec.com?subject=demande%20de%20contact&body=Bonjour,%0D%0A%0D%0AJe%20suis%20intéressé%20et%20je%20souhaiterai%20obtenir%20plus%20d’informations%20concernant%20votre%20matériel.%0D%0A%0D%0AMerci%20de%20me%20contacter%0D%0APar%20téléphone%20:%20%200561429063%0D%0APar%20mail%20:%20contact@agm-tec.com";
-window.close();  // Ferme la page après avoir effectué la redirection
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("Script chargé");
+  window.location.href = "mailto:contact@agm-tec.com?subject=demande%20de%20contact&body=Bonjour,%0D%0A%0D%0AJe%20suis%20intéressé%20et%20je%20souhaiterai%20obtenir%20plus%20d’informations%20concernant%20votre%20matériel.%0D%0A%0D%0AMerci%20de%20me%20contacter%0D%0APar%20téléphone%20:%20%200561429063%0D%0APar%20mail%20:%20contact@agm-tec.com";
+  window.close();
+});
